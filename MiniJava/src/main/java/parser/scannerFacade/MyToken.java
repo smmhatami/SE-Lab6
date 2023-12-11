05f4965e968cf588e3a457cc2cd66fc95ed62182
smmhatami <hatamik@gmail.com>
2023-12-11 22:16:43 +0330

    refactor : Remove Redundant code in MyToken

--- MiniJava/src/main/java/parser/scannerFacade/MyToken.java
+++ MiniJava/src/main/java/parser/scannerFacade/MyToken.java
@@ -34,26 +34,7 @@ public class MyToken {
     }
 
     public static Type getTyepFormString(String s) {
-        Pattern pattern;
-        Matcher matcher;
-        for (Type t : Type.values()) {
-            if (t.toString().equals(s)) return t;
-        }
-        for (Type t : Type.values()) {
-            pattern = Pattern.compile(t.pattern);
-            matcher = pattern.matcher(s);
-            if (matcher.matches()) return t;
-        }
-
-//        if (s.equals("class")||s.equals("extends")||s.equals("public")||s.equals("static")||s.equals("void")||s.equals("return")||s.equals("main")||
-//                s.equals("boolean")||s.equals("int")||s.equals("if")||s.equals("else")||s.equals("while")||s.equals("true")||s.equals("false")||s.equals("System.out.println")) {
-//            return KEYWORDS;
-//        }else if(s.equals("")){
-//
-//        }else if(s.equals("")){
-//
-//        }
-        throw new IllegalArgumentException();
+        return Token.getTyepFormString(s);
     }
 
     public Token getToken() {
